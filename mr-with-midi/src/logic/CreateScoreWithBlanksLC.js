c2db0f54f6d1d71134a506c9b4cbeb7b86421de6
Yixiao Chen <ychen239@uottawa.ca>
2019-04-16 19:20:42 -0400

    minor updates

--- mr-with-midi/src/logic/CreateScoreWithBlanksLC.js
+++ mr-with-midi/src/logic/CreateScoreWithBlanksLC.js
@@ -2,6 +2,7 @@
 //read score arrays and pass them onto child for display cycle.
 import React from "react";
 import DisplayLC from "./DisplayLC";
+import score1 from "../score/score-1";
 
 
 class CreateScoreWithBlanksLC extends React.Component {
@@ -16,19 +17,13 @@ class CreateScoreWithBlanksLC extends React.Component {
           staffWidth: "100",
           stayTime: 1000,
         };
+        this.abcParam=score1;
 
-
-        this.abcParam = [];
-        this.abcParam[0]={score:"", stayTime:1000, imageSizeFactor:"", staffWidth:""};
-        this.abcParam[1]={score:"|:d2|EB{c}BA B2 EB|", stayTime:3000, imageSizeFactor:"3.2", staffWidth:"240"};
-        this.abcParam[2]={score:"", stayTime:1000, imageSizeFactor:"", staffWidth:""};
-        this.abcParam[3]={score:"|D2 a|",  stayTime:500, imageSizeFactor:"1.2", staffWidth:"440"};
-        this.abcParam[4]={score:"", stayTime:1000, imageSizeFactor:"", staffWidth:""};
-        this.abcParam[5]={score:"|:b2 C|", stayTime:3000, imageSizeFactor:"3.2", staffWidth:"340"};
 
     }
     handleKeyDown(event){
         if (event.key === '1') {
+            console.log(this.abcParam);
             //for loop, setState, async
             for (let i=0,accum=this.abcParam[0]["stayTime"];i<this.abcParam.length;accum+=this.abcParam[++i]["stayTime"]) {
 
@@ -70,15 +65,3 @@ class CreateScoreWithBlanksLC extends React.Component {
 
 
 export default CreateScoreWithBlanksLC;
-
-/*
-* <Button1><Button2><Button3>
-*
-*
-*
-*
-*
-*
-*
-*
-* */
--- mr-with-midi/src/score/score-1.js
+++ mr-with-midi/src/score/score-1.js
@@ -1,11 +1,11 @@
-export const score1 = [
-    {score:"|:d2|EB{c}BA B2 EB|", blankTime:1000, stayTime:3000, imageSizeFactor:"3.2", staffWidth:"240"},
-    {score:"|D2 a|", blankTime:1500, stayTime:500, imageSizeFactor:"1.2", staffWidth:"440"},
-    {score:"", stayTime:1000, imageSizeFactor:"", staffWidth:""},
-    {score:"|D2 a|",  stayTime:500, imageSizeFactor:"1.2", staffWidth:"440"},
-    {score:"", stayTime:1000, imageSizeFactor:"", staffWidth:""},
-    {score:"|:b2 C|", stayTime:3000, imageSizeFactor:"3.2", staffWidth:"340"}
-]
+const score1 = [
+{score:"", imageSizeFactor:"", staffWidth:"", stayTime:1000},
+{score:"|:d2|EB{c}BA B2 EB|", imageSizeFactor:"3.2", staffWidth:"240" , stayTime:3000},
+{score:"", imageSizeFactor:"", staffWidth:"" , stayTime:1000},
+{score:"|D2 a|", imageSizeFactor:"1.2", staffWidth:"440",  stayTime:500},
+{score:"", imageSizeFactor:"", staffWidth:"", stayTime:1000},
+{score:"|:b2 C|", imageSizeFactor:"3.2", staffWidth:"340", stayTime:3000}
+];
 
 export default {
     score1,
